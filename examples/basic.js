66815189e27caced5fe4b19578545294c4bf4164
Edward Hotchkiss <edward@edwardhotchkiss.com>
2012-03-04 11:41:12 -0500

    [lets enhance that!] !logs /myname/i

--- examples/basic.js
+++ examples/basic.js
@@ -5,23 +5,23 @@ var irc = require('../lib/irc');
 var bot = new irc.Client();
 
 bot.on('ready', function() {
-  console.log('connected to irc...');  
+  console.log('CONNECTED >'.green);  
 });
 
 bot.on('joined', function(channel) {
-  console.log('JOINED ' + channel);
+  console.log('JOINED > '.green + channel);
 });
 
 bot.on('privateMessage', function(from, message) {
-  console.log('PRIVATE MESSAGE > %s: %s', from, message);
+  console.log('PRIVATE MESSAGE > '.green + from + ': ' + message);
 });
 
 bot.on('channelMessage', function(from, message) {
-  console.log('CHANNEL MESSAGE > %s: %s', from, message);
+  console.log('CHANNEL MESSAGE > '.green + from + ': ' + message);
 });
 
 bot.on('command', function(command) {
-  console.log('COMMAND: %s', command);
+  console.log('COMMAND > '.green + command);
 });
 
 /* EOF */
