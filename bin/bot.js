97b0c85417baea831bef75b86e5e4cde7966f976
Edward Hotchkiss <edward@edwardhotchkiss.com>
2012-03-03 23:14:21 -0500

    [commands] !help

--- bin/bot.js
+++ bin/bot.js
@@ -16,15 +16,19 @@ bot.on('ready', function() {
 });
 
 bot.on('joined', function(channel) {
-  console.log('joined ' + channel);
+  console.log('JOINED ' + channel);
 });
 
 bot.on('privateMessage', function(from, message) {
-  console.log('PRIVATE message > %s: %s', from, message);
+  console.log('PRIVATE MESSAGE > %s: %s', from, message);
 });
 
 bot.on('channelMessage', function(from, message) {
-  console.log('CHANNEL message > %s: %s', from, message);
+  console.log('CHANNEL MESSAGE > %s: %s', from, message);
+});
+
+bot.on('command', function(command) {
+  console.log('COMMAND: %s', command);
 });
 
 /* EOF */
